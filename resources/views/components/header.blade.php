cbc65688ce967984ed6d6ad61cd6f72c0e76d322
Yari <narducciyarigmail.com>
2023-01-30 17:32:29 +0100

    aggiunta img

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="container">
+  
+   
+      <nav>
+        <div id="img-container">
+          <img src="img/dc-logo.png" alt="Logo DC">
+        </div>
+        <ul>
+          <li>
+            <a href="#">Link</a>
+        </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
