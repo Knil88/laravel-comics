2b8d6ab92bd81baf12e35a3dbdbcfa34120e07ce
Yari <narducciyarigmail.com>
2023-01-30 18:09:40 +0100

    divisione scss

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,12 +4,19 @@
    
       <nav>
         <div id="img-container">
-          <img src="img/dc-logo.png" alt="Logo DC">
+          <img src="" alt="Logo DC">
         </div>
         <ul>
-          <li>
-            <a href="#">Link</a>
-        </li>
+            <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
         </ul>
       </nav>
     </div>
